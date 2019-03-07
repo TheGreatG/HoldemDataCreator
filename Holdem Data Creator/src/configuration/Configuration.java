e25bd803cf6dc9ce56fb1d70b5e50db08f2fdcb6
maros <maros@DESKTOP-M5STHDH>
2019-03-07 15:08:26 +0100

    Added data folder.

--- Holdem Data Creator/src/configuration/Configuration.java
+++ Holdem Data Creator/src/configuration/Configuration.java
@@ -3,19 +3,31 @@ package configuration;
 import java.io.File;
 
 public class Configuration {
-	//private static String handHistory = "C:/PokerPredictor/HandHistory/";
-	private static String handHistory = "C:/PokerPredictor/HandHistory/";
-	private static String pokerPredictorFolderPath = "C:/PokerPredictor/";
-	private static String clusteringFolderPath = "C:/PokerPredictor/Clustering/";
-	private static String clusteringFilePath = "C:/PokerPredictor/Clustering/clusteringInputForR.csv";
-	private static String clusteredPlayersGroupsDirectoryPath = "C:/PokerPredictor/ClusteringGroups";
-	private static String playersOver15000FilePath = "C:/PokerPredictor/Clustering/playersOver15000.csv";
-	private static String statsForPredictionRegularsDirectoryPath = "C:/PokerPredictor/Prediction/"; 
-	private static String statsForPredictionGroupsDirectoryPath = "C:/PokerPredictor/PredictionGroups/";
-	private static String handToPredictDirectoryPath = "C:/PokerPredictor/HandToPredict/";
-	private static String modelSaverDirectoryPath = "C:/PokerPredictor/ModelSaver/";
-	private static String modelSaverEmptyDirectoryPath = "C:/PokerPredictor/ModelSaverEmpty/";
-	private static String modelsPlotDirectoryPath = "C:/PokerPredictor/ModelsPlots/";
+//	private static String handHistory = "C:/PokerPredictor/HandHistory/";
+//	private static String pokerPredictorFolderPath = "C:/PokerPredictor/";
+//	private static String clusteringFolderPath = "C:/PokerPredictor/Clustering/";
+//	private static String clusteringFilePath = "C:/PokerPredictor/Clustering/clusteringInputForR.csv";
+//	private static String clusteredPlayersGroupsDirectoryPath = "C:/PokerPredictor/ClusteringGroups";
+//	private static String playersOver15000FilePath = "C:/PokerPredictor/Clustering/playersOver15000.csv";
+//	private static String statsForPredictionRegularsDirectoryPath = "C:/PokerPredictor/Prediction/"; 
+//	private static String statsForPredictionGroupsDirectoryPath = "C:/PokerPredictor/PredictionGroups/";
+//	private static String handToPredictDirectoryPath = "C:/PokerPredictor/HandToPredict/";
+//	private static String modelSaverDirectoryPath = "C:/PokerPredictor/ModelSaver/";
+//	private static String modelSaverEmptyDirectoryPath = "C:/PokerPredictor/ModelSaverEmpty/";
+//	private static String modelsPlotDirectoryPath = "C:/PokerPredictor/ModelsPlots/";
+	
+	private static String handHistory = "Data/HH jan jul 2016/";
+	private static String pokerPredictorFolderPath = "Data/PokerPredictor/";
+	private static String clusteringFolderPath = "Data/PokerPredictor/Clustering/";
+	private static String clusteringFilePath = "Data/PokerPredictor/Clustering/clusteringInputForR.csv";
+	private static String clusteredPlayersGroupsDirectoryPath = "Data/PokerPredictor/ClusteringGroups";
+	private static String playersOver15000FilePath = "Data/PokerPredictor/Clustering/playersOver15000.csv";
+	private static String statsForPredictionRegularsDirectoryPath = "Data/PokerPredictor/Prediction/"; 
+	private static String statsForPredictionGroupsDirectoryPath = "Data/PokerPredictor/PredictionGroups/";
+	private static String handToPredictDirectoryPath = "Data/PokerPredictor/HandToPredict/";
+	private static String modelSaverDirectoryPath = "Data/PokerPredictor/ModelSaver/";
+	private static String modelSaverEmptyDirectoryPath = "Data/PokerPredictor/ModelSaverEmpty/";
+	private static String modelsPlotDirectoryPath = "Data/PokerPredictor/ModelsPlots/";
 	
 	private static int maximumActionsInTheGame = 40; //dont store hands with more actions
 	
